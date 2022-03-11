6fee0b05d6a39c148cc631dfaa3f58f9e83b3144
Ashley Ryan <asryan@vmware.com>
2022-03-11 11:39:28 -0500

    Update unit tests with CdsDropdown failure

--- create-react-app/src/App.test.tsx
+++ create-react-app/src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
+import { CdsDropdown } from '@cds/react/dropdown';
 
 test('renders learn react link and button', async () => {
   render(<App />);
@@ -9,4 +10,19 @@ test('renders learn react link and button', async () => {
 
   const buttonElement = await screen.findByRole('button', { name: 'Show Alert' });
   expect(buttonElement).toBeInTheDocument();
+});
+
+const TestComponent = () => {
+  const ref = React.useRef<HTMLSpanElement>();
+
+  return (
+    <>
+      <span ref={ref}>Anchor Element</span>
+      <CdsDropdown anchor={ref.current}></CdsDropdown>
+    </>
+  );
+};
+
+test('renders CdsDropwn', () => {
+  render(<TestComponent />);
 });
